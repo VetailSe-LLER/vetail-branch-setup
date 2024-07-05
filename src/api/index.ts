2b17ca20a81c2b041b98fe4278b25e742b7d02c0
Paing Thura Kyaw <kyawna2265@gmail.com>
2024-07-05 08:59:07 +0630

    feat: changes api

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import { token } from "@/store/client/store";
 import Axios from "axios";
 
 export const axios = Axios.create({
-  baseURL: "http://192.168.0.133/VetailAuth_API/api/",
+  baseURL: "https://mysterious.shopdoora.com/vetailAuth/api/",
 });
 
 export const authJsonHeader = (file?: boolean) => {
